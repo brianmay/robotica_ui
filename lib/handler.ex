363ab9e33041e5a916e11fa1f82890cf2f424789
Brian May <brian@linuxpenguins.xyz>
2018-12-06 08:14:44 +1100

    Fix variable unused warnings in handler

--- lib/handler.ex
+++ lib/handler.ex
@@ -7,7 +7,7 @@ defmodule RoboticaUi.Handler do
     {:ok, args}
   end
 
-  def connection(status, state) do
+  def connection(_status, state) do
     # `status` will be either `:up` or `:down`; you can use this to
     # inform the rest of your system if the connection is currently
     # open or closed; tortoise should be busy reconnecting if you get
@@ -27,18 +27,18 @@ defmodule RoboticaUi.Handler do
     {:ok, state}
   end
 
-  def handle_message(topic, payload, state) do
+  def handle_message(_topic, _payload, state) do
     # unhandled message! You will crash if you subscribe to something
     # and you don't have a 'catch all' matcher; crashing on unexpected
     # messages could be a strategy though.
     {:ok, state}
   end
 
-  def subscription(status, topic_filter, state) do
+  def subscription(_status, _topic_filter, state) do
     {:ok, state}
   end
 
-  def terminate(reason, state) do
+  def terminate(_reason, _state) do
     # tortoise doesn't care about what you return from terminate/2,
     # that is in alignment with other behaviours that implement a
     # terminate-callback
