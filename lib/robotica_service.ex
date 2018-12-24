c0be8fe46abeee2b2df944b2f0c5e4e6d0aad0cc
Brian May <brian@linuxpenguins.xyz>
2018-12-24 15:10:12 +1100

    Handle done event

--- lib/robotica_service.ex
+++ lib/robotica_service.ex
@@ -15,7 +15,7 @@ defmodule RoboticaUi.RoboticaService do
     GenServer.cast(__MODULE__, event)
   end
 
-  def handle_cast({topic, id}, state) do
+  def handle_cast({:execute=topic, id}, state) do
     data = EventBus.fetch_event_data({topic, id})
 
     case data.message.text do
@@ -28,8 +28,13 @@ defmodule RoboticaUi.RoboticaService do
 
     EventBus.mark_as_completed({RoboticaService, topic, id})
 
-    Process.sleep(5000)
+#    Process.sleep(500)
 
+    {:noreply, state}
+  end
+
+  def handle_cast({:done=topic, id}, state) do
+    EventBus.fetch_event_data({topic, id})
     RoboticaUi.RootManager.set_scene(:message, nil)
     {:noreply, state}
   end
